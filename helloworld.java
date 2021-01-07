de501ab6522bb2493cc310690663f286cfe7556f
larven313 <77083930+larven313@users.noreply.github.com>
2021-01-07 10:53:07 +0700

    Update helloworld.java
    menambahkan nama dan umur

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,10 @@
 public class Helloworld {
 	public static void main(){
 		System.out.println("Hello World");
+		String nama = :"Lare";
+		int umur = 10;
+		
+		System.out.println("Nama "+nama);
+		System.out.println("Umur "+umur);
 	}
 }
