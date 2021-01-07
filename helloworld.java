d9c8c08505d71ed838b0fe0a5617cefb0bebb588
larven313 <77083930+larven313@users.noreply.github.com>
2021-01-07 10:41:38 +0700

    Update helloworld.java
    di update

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,10 @@
 public class Helloworld {
 	public static void main(){
 		System.out.println("Hello World");
+		
+	String nama = "zamzam";
+		int umur = 20;
+		System.out.println("Nama "+nama);
+		System.out.println("Umur "+umur);
 	}
 }
